c0315ad0950fe885747c6724b9ae1eb55ff255eb
Martin Brown <Martin.Brown@state.or.us>
2019-12-26 14:16:22 -0800

    flesh out new folders somewhat

--- wic-app/app.R
+++ wic-app/app.R
@@ -1,50 +1,201 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# file app2.R
+# rough draft of waste impact calculator app
 
+# it's going well so far.ay of processing this.
+# maybe that's for when I return to it.
+
+# i need to think about the most efficient w
+# let's do this systematically
+
+# to make the final graph, i need this data frame:
+# impact by impactCategory - scenario - LCstage 
+
+# that comes from summing up this data frame:
+# impact by impactCategory - scenario - material - LCstage
+
+# that in turn comes from a combination of two subsets
+# material impact by impactCategory - scenario - material - LCstage
+# [using tons]
+# and
+# transport impact by impactCategory - scenario - material
+# [using mileage and tons with LCstage set to "endOfLifeTransport"]
+
+# so really when i do the pivot, i need to create a tons
+# variable and a mileage variable.
+
+
+# here is the code to pivot it
+# junk <- 
+# pivot_longer(mat_disp_combos,baseline_tons:alternative_miles, 
+# names_to=c("scenario","thing"), names_sep="_",  
+# values_to=c("quant")) %>% 
+# pivot_wider(values_from = "quant", names_from = "thing")
+
+
+# i've been working to get end of life transport in there
+# but right now it's mistakenly characterized as tons
+
+
+# load packages
 library(shiny)
+library(tidyverse)
+library(DT)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
+# load data
+no_records <- 2
+impact_factors <-
+  readRDS(
+    "../oregon_deq/projects/arr_scenarios_deq_factors/intermediate_output/impact_factors_deq.Rdata"
+    ) %>%
+  as.data.frame()
+mat_disp_combos <- 
+  impact_factors %>%
+  filter(LCstage == "endOfLife") %>% 
+  sample_n(no_records) %>%
+  unique() %>%
+  select(material, disposition, impliedMiles) %>%
+  mutate(
+    baseline=round(runif(n=no_records, min=0, max=10),1),
+    baselineMiles=impliedMiles,
+    alternative=round(runif(n=no_records, min=0, max=10),1),
+    alternativeMiles=impliedMiles
+  ) %>%
+  select(-impliedMiles) %>%
+  as.data.frame()
+
+# user interface
+ui <- 
+  fluidPage(
+    tabsetPanel(
+      tabPanel(title="something"),
+      tabPanel(
+        title="enter your own",
+        sidebarLayout(
+          sidebarPanel(
+            selectInput(
+              inputId="eyoImpactCategoryChoice",
+              label="impact category",
+              choices = unique(impact_factors$impactCategory),
+              selected="Smog"
+            ),
+            h2("the original table"),
+            tableOutput("x0"),
+            h2("An editable table"),
+            DTOutput("x1"),
+            h2("Edits preserved and transformed"),
+            DTOutput("x2"),
+            h2("tons"),
+            tableOutput("x2a"),
+            h2("mileage"),
+            tableOutput("x2b"),
+            h2("combined"),
+            tableOutput("x2c"),
+            width=6
+          ),
+          mainPanel(
+            h2("Edits used in graphic output"),
+            plotOutput(outputId="x3"),
+            h2("table with impacts"),
+            tableOutput("x4"),
+            width=6
+          )
+        )
       ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
-)
+      tabPanel(title="something else")
+    )
+  ) #close fluidpage
+# end ui definition
 
-# Define server logic required to draw a histogram
+# server
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  
+  output$x0 <- renderTable(mat_disp_combos)
+  
+  output$x1 <- renderDT(mat_disp_combos, 
+                        selection = 'none',
+                        editable = TRUE
+  )
+  
+  proxy <- dataTableProxy('x1')
+  
+  observeEvent(input$x1_cell_edit, {
+    info <- input$x1_cell_edit
+    str(info)
+    i <- info$row
+    j <- info$col
+    v <- info$value
+    
+    #limits editing to columns specified by j
+    if ( j >=3 & j <= 6) {
+      mat_disp_combos[i, j] <<- DT::coerceValue(v, mat_disp_combos[i, j])
+      replaceData(proxy, mat_disp_combos, resetPaging = FALSE)  # important
+    } else {}
+    mat_disp_combos
+    
+  })
+  
+  y <- reactive({
+    input$x1_cell_edit
+    # here is where the joins and so on would go in
+    mat_disp_combos %>% mutate(baseline=baseline*10) %>%
+      pivot_longer(
+        baseline:alternativeMiles,
+        names_to="tonnageType",
+        values_to="tons"
+      )
+  })
+  
+  ya <- reactive({
+    filter(
+      y(),
+      tonnageType=="baseline" | tonnageType=="alternative"
+    ) %>%
+      mutate(LCstage="endOfLife")
+  })
+  
+  yb <- reactive({
+    filter(
+      y(),
+      tonnageType=="baselineMiles" | tonnageType=="alternativeMiles"
+    ) %>%
+      mutate(LCstage="endOfLifeTransport")
+  })
+  
+  yc <- reactive({
+    bind_rows(ya(),yb()) %>%
+      arrange(material, LCstage, disposition)
+  })
+  
+  output$x2 <- renderDT(y(),
+                        selection = 'none')
+  
+  output$x2a <- renderTable(ya())
+  output$x2b <- renderTable(yb())
+  output$x2c <- renderTable(yc())
+  
+  
+  output$x3 <- renderPlot({
+    ggplot()+
+      geom_point(
+        data=y(),
+        aes(x=material, y=tons, fill=tonnageType, color=tonnageType),
+        size=5
+      )
+  })
+  
+  z <- reactive({
+    left_join(
+      y(),
+      impact_factors,
+      by=c("material", "disposition")
+    ) %>%
+      mutate(impact=tons*impactFactor)
+  })
+  
+  output$x4 <- renderTable(
+    z() %>% filter(impactCategory==input$eyoImpactCategoryChoice)
+  )
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
