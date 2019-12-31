33c32e978b90f451773d50ea319e4df9b3cc251e
Martin Brown <Martin.Brown@state.or.us>
2019-12-30 19:18:14 -0800

    improve theme_539

--- oregon_deq/resources/theme_539.R
+++ oregon_deq/resources/theme_539.R
@@ -1,11 +1,25 @@
+# file theme_539.R
+
+# this file creates a variant of ggthemes' "fivethirtyeight" theme
+# with a transparent background and a few other details changed.
+# this theme is a good match for DEQ's official style guidelines,
+# which require an Arial/Helvetica font
+
+# it also defines a color palette based on DEQ preferred colors, 
+# converted to R color names.
+
+# creating a custom theme
+theme_539 <- function() {
+  theme_fivethirtyeight() +
+  theme(rect=element_rect(fill="transparent"))
+}
+
 # creating a palette using "official" Oregon DEQ colors
 # for colors and fills
 deq_pal <- 
   c("aquamarine4", "steelblue4", "lightseagreen", 
-		 "yellowgreen", "sienna1", "darkseagreen", 
-			"slateblue2", "powderblue", "khaki", "darksalmon", 
-			"dodgerblue4", "darkslateblue", "maroon4", "cyan4", 
-			"darkgoldenrod3")
+    "yellowgreen", "sienna1", "darkseagreen", 
+    "slateblue2", "powderblue", "khaki", "darksalmon", 
+    "dodgerblue4", "darkslateblue", "maroon4", "cyan4", 
+    "darkgoldenrod3")
 names(deq_pal) <- deq_pal
-
-# creating a custom theme
